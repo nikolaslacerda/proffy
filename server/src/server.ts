b3815a3d1f282b29ac069c5730016ae77acff6f6
nikolaslacerda <nikolas.lacerda@hotmail.com>
2020-08-05 22:20:19 -0300

    add database and tables

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ "hello": "world" }))
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello World'})
+})
 
 app.listen(3333);

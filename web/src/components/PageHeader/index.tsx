915529ff8bc16258862e6aaef253c627fb2ff6af
nikolaslacerda <nikolas.lacerda@hotmail.com>
2020-08-06 19:17:05 -0300

    add give classes page

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,25 +8,27 @@ import './styles.css';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) =>  {
     return (
         <header className="page-header">
-               <div className="top-bar-container">
-                   <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                   </Link>
-                   <img src={logoIcon} alt="Proffy"/>
-               </div>
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Voltar"/>
+                </Link>
+                <img src={logoIcon} alt="Proffy"/>
+            </div>
 
-               <div className="header-content">
-                   <strong>{props.title}</strong>
-                   {props.children}
-               </div>
+            <div className="header-content">
+                <strong>{props.title}</strong>
+                { props.description && <p>{props.description}</p> }
 
-               
-           </header>
+                {props.children}
+            </div>
+
+        </header>
     );
 }
 
